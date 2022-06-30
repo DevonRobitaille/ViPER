65d0099dc29a5227c5855cf5f44eb96ee2a916cb
DevonRobitaille <devon.robitaille+github@protonmail.com>
2022-06-30 12:40:25 -0400

    Changed File Structure for Modals

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,30 +1,17 @@
 import { useRouter } from "next/router"
 import { useState } from "react"
+import AddUserModal from "../components/modal/addUserModal"
 import { useUserContext } from "../context/user.context"
 import { trpc } from "../utils/trpc"
-import { Dialog } from '@headlessui/react'
-import { useForm } from 'react-hook-form'
-import { CreateUserInput } from '../schema/user.schema';
 
 export function AdminPage() {
     const user = useUserContext()
     const router = useRouter()
 
+    const [addUserModalIsOpen, setAddUserModalIsOpen] = useState(false);
     const [companyModalIsOpen, setCompanyModalIsOpen] = useState(false);
     const [jobModalIsOpen, setJobModalIsOpen] = useState(false);
     const [vendorModalIsOpen, setVendorModalIsOpen] = useState(false);
-
-    // User Modal
-    const [addUserModalIsOpen, setAddUserModalIsOpen] = useState(false);
-    const { handleSubmit: addUserHandleSubmit, register: addUserRegister, reset: addUserReset } = useForm<CreateUserInput>()
-    const { mutate, error } = trpc.useMutation(['users.register-user'], {
-        onSuccess: () => {
-            setAddUserModalIsOpen(false)
-        },
-    })
-    const onUserSubmit = (values: CreateUserInput) => {
-        addUserReset()
-    }
 
     if (user?.role !== 'Admin') router.push('/')
     else {
@@ -33,42 +20,7 @@ export function AdminPage() {
         return (
             <>
                 {/* Add User Modal */}
-                <Dialog className="flex mx-auto absolute z-50" open={addUserModalIsOpen} onClose={() => setAddUserModalIsOpen(false)}>
-                    {/* The backdrop, rendered as a fixed sibling to the panel container */}
-                    <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-
-                    {/* Full-screen container to center the panel */}
-                    <div className="fixed inset-0 flex items-center justify-center p-4">
-                        <Dialog.Panel className="flex flex-col bg-white px-10 py-5 items-center">
-                            {/* title */}
-                            <Dialog.Title className='text-2xl font-semibold mb-4'>Add User</Dialog.Title>
-
-                            {/* body */}
-                            < p className='text-red-500 text-center font-semibold text-lg' > {error && error.message} </p>
-                            <form onSubmit={addUserHandleSubmit(onUserSubmit)} className='mx-auto flex flex-col content-center justify-center items-center gap-2' >
-
-                                {/* Fields */}
-                                <div className='inputDiv'>
-                                    <input {...addUserRegister('firstName')} className='inputField' type='text' placeholder='First Name' />
-                                </div>
-                                <div className='inputDiv'>
-                                    <input {...addUserRegister('lastName')} className='inputField' type='text' placeholder='Last Name' />
-                                </div>
-                                <div className='inputDiv'>
-                                    <input {...addUserRegister('email')} className='inputField' type='email' placeholder='Email' />
-                                </div>
-
-                                {/* buttons for submitting */}
-                                <div className='flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b'>
-                                    <button onClick={() => setAddUserModalIsOpen(false)} className='text-red-500 hover:text-[#f7498e] mt-1 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>Cancel</button>
-                                    <button type="submit" onClick={() => setAddUserModalIsOpen(false)} className='btn uppercase font-bold'>Create</button>
-                                </div>
-                            </form>
-
-
-                        </Dialog.Panel>
-                    </div>
-                </Dialog>
+                <AddUserModal isOpen={addUserModalIsOpen} setIsOpen={setAddUserModalIsOpen} />
                 <div className='grid mt-10 grid-cols-1 gap-8 mx-20 md:grid-cols-2'>
                     {/* Users Section */}
                     <section className='flex-col'>
